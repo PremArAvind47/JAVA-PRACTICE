0964685248d1d51963f4380f2493ae8fcbae1931
PremArAvind47 <premaravind47w@gmail.com>
2026-02-20 15:56:28 +0530

    added

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,3 +1,5 @@
+package test;
+
 import java.io.*;
 
 class UpperFilterReader extends FilterReader {
---
+++ src/test/SubsetsBit.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubsetsBit {
+
+	static List<String> subsetsBit(String s) {
+		List<String> l1 = new ArrayList<>();
+		List<String> l2 = new ArrayList<>();
+		List<String> result = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			sb.append(String.valueOf(s.charAt(i)));
+			for (int j = i + 1; j < s.length(); j++) {
+				sb.append(String.valueOf(s.charAt(j)));
+				l1.add(sb.toString());
+				boolean contains = l2.contains(sb.toString());
+				if (contains) {
+					result.add(sb.toString());
+				}
+			}
+			l2.addAll(l1);
+			l1.clear();
+			sb.delete(0, 1000);
+		}
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		String s = "abcabc";
+		System.out.println(subsetsBit(s));
+	}
+
+}

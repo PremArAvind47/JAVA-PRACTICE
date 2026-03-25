7dc7745a95068f189b82f67e444a0d7994e84f83
PremArAvind47 <premaravind47w@gmail.com>
2026-03-25 17:41:28 +0530

    added

--- src/test/LongestZigZag.java
+++ src/test/LongestZigZag.java
@@ -1,69 +1,55 @@
 package test;
 
 public class LongestZigZag {
+    LongestZigZag left;
+    LongestZigZag right;
+    int val;
 
-	static class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
+    LongestZigZag(int val) {
+        this.val = val;
+    }
 
-		TreeNode(int val) {
-			this.val = val;
-		}
-	}
+    static int max = 0;
 
-	static int max = 0;
+    public static int method(LongestZigZag node) {
+        max = 0;
+        dfs(node, true, 0);
+        return max;
+    }
 
-	public static int longestZigZag(TreeNode root) {
-		dfs(root, true, 0); // start by going left
-		dfs(root, false, 0); // start by going right
-		return max;
-	}
+    static void dfs(LongestZigZag node, boolean left, int length) {
+        if (node == null) return;
 
-	
-	
-	
-	
-	public static void dfs(TreeNode node, boolean goLeft, int length) {
-		
-		if (node == null)
-			return;
+        max = Math.max(max, length);
 
-		max = Math.max(max, length);
+        if (left) {
+            dfs(node.left, false, length + 1);
+            dfs(node.right, true, 1);
+        } else {
+            dfs(node.right, true, length + 1);
+            dfs(node.left, false, 1);
+        }
+    }
 
-		if (goLeft) {
-			dfs(node.left, false, length + 1); // continue zigzag
-			dfs(node.right, true, 1); // restart
-		} else {
-			dfs(node.right, true, length + 1); // continue zigzag
-			dfs(node.left, false, 1); // restart
-		}
-	}
+    public static void main(String[] args) {
 
-	
-	
-	
-	
-	
-	
-	public static void main(String args[]) {
+        //        1
+        //       /
+        //      2
+        //     /
+        //    3
+        //     \
+        //      4
+        //     /
+        //    5
 
-		// 5
-		// / \
-		// 3 8
-		// / / \
-		// 1 7 9
+        LongestZigZag root = new LongestZigZag(1);
 
-		TreeNode root = new TreeNode(5);
-		root.left = new TreeNode(3);
-		root.right = new TreeNode(8);
+        root.left = new LongestZigZag(2);
+        root.left.left = new LongestZigZag(3);
+        root.left.left.right = new LongestZigZag(4);
+        root.left.left.right.left = new LongestZigZag(5);
 
-		root.left.left = new TreeNode(1);
-
-		root.right.left = new TreeNode(7);
-		root.right.right = new TreeNode(9);
-
-		int result = longestZigZag(root);
-		System.out.println("ZigZag Length: " + result);
-	}
+        System.out.println("ZigZag Length: " + method(root));
+    }
 }
